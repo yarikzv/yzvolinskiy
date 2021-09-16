6f9884108e46e9c1b61ffe00e69cb3fd0a3a05ec
Class9-PC10 <yzv.yaroslav@gmail.com>
2021-09-16 21:22:58 +0300

    Lesson 10

---
+++ src/lesson10/Employee.java
@@ -0,0 +1,15 @@
+package lesson10;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String surname;
+    private int departmentId;
+
+    public Employee(int id, String name, String surname, int departmentId) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.departmentId = departmentId;
+    }
+}
---
+++ src/lesson10/InnerJoinDemo.java
@@ -0,0 +1,9 @@
+package lesson10;
+
+public class InnerJoinDemo {
+    
+    public static Employee convertToEmployee(String str) {
+        String[] arr = str.split(";");
+        return new Employee(Integer.parseInt(arr[0]), arr[1], arr[2], Integer.parseInt(arr[3]));
+    }
+}
