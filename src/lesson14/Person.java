40f5c5ce27f226aba64ff9e02b68c3ef8465277f
John Doe <johndoe@example.com>
2021-09-30 21:23:15 +0300

    Lesson 12

---
+++ src/lesson14/Person.java
@@ -0,0 +1,14 @@
+package lesson14;
+
+import lombok.*;
+
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@Getter
+@Setter
+public class Person {
+    private Integer id;
+    private String name;
+    private String email;
+}
---
+++ src/lesson14/lesson14.java
@@ -0,0 +1,8 @@
+package lesson14;
+
+public class lesson14 {
+    public static void main(String[] args) {
+        Person p = new Person(10,"Mike", "mike@test.com");
+        System.out.println(p);
+    }
+}
