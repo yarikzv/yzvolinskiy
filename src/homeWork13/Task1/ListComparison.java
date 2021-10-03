fdc3f2d5ab642fd870ff5471d28973e3a21e7ff6
yarikzv <yzv.yaroslav@gmail.com>
2021-10-03 23:13:53 +0300

    HW-Lesson13

---
+++ src/homeWork13/Task1/ListComparison.java
@@ -0,0 +1,80 @@
+package homeWork13.Task1;
+
+import java.util.*;
+
+/**
+ * A simple program for comparison ArrayList and LinkedList add and get operations.
+ */
+public class ListComparison {
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
+
+        addMethod(arrayList);
+
+        addMethod(linkedList);
+
+        getMethodUsingShuffle(arrayList);
+
+        getMethodUsingMathRandom(linkedList);
+
+        //getMethod(linkedList);          //takes about 4 minutes on my PC
+    }
+
+    /**
+     * Allows adding to any List Integer numbers from 0 to 1_000_000 and calculates
+     * elapsed time.
+     *
+     * @param list List for adding elements.
+     */
+    public static void addMethod(List<Integer> list) {
+        double start, end;
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            list.add(i);
+        }
+        end = System.currentTimeMillis();
+        System.out.printf("Add to %s time: %s ms\n", list.getClass().getSimpleName(), (end - start));
+    }
+
+    /**
+     * Allows to get any 100_000 elements by Math.random() method and calculates
+     * elapsed time. Repeating of indexes is allowed.
+     *
+     * @param list List of items for getting.
+     */
+    public static void getMethodUsingMathRandom(List<Integer> list) {
+        double start, end;
+        start = System.currentTimeMillis();
+        List<Integer> copy = new ArrayList<>(list);
+        for (int i = 0; i < 100_000; i++) {
+            copy.add((int) (Math.random() * 1_000_000));
+        }
+        for (int i = 0; i < 100_000; i++) {
+            list.get(copy.get(i));
+        }
+        end = System.currentTimeMillis();
+        System.out.printf("Get from %s time: %s ms\n", list.getClass().getSimpleName(), (end - start));
+    }
+
+    /**
+     * Allows to get any 100_000 elements by creating a copy of list and
+     * shuffling items and calculates elapsed time. Then getting items by indexes
+     * from shuffled list. It allows avoiding repeating indexes.
+     * But take more time... A lot of time. 10 times more than getMethodUsingMathRandom().
+     *
+     * @param list List of items for getting.
+     */
+    public static void getMethodUsingShuffle(List<Integer> list) {
+        double start, end;
+        start = System.currentTimeMillis();
+        List<Integer> copy = new ArrayList<>(list);
+        Collections.shuffle(copy);
+        for (int i = 0; i < 100_000; i++) {
+            list.get(copy.get(i));
+        }
+        end = System.currentTimeMillis();
+        System.out.printf("Get from %s time: %s ms\n", list.getClass().getSimpleName(), (end - start));
+    }
+}
+
---
+++ src/homeWork13/Task2/Box.java
@@ -0,0 +1,45 @@
+package homeWork13.Task2;
+
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Box {
+    private int height;
+    private int width;
+    private int length;
+    private int weight;
+
+    public Box(int height, int width, int length, int weight) {
+        this.height = height;
+        this.width = width;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Box box = (Box) o;
+        return height == box.height && width == box.width && length == box.length && weight == box.weight;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(height, width, length, weight);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Box.class.getSimpleName() + "[", "]")
+                .add("height=" + height)
+                .add("width=" + width)
+                .add("length=" + length)
+                .add("weight=" + weight)
+                .toString();
+    }
+}
