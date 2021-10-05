ed52c384018b59c0853583491ac0c5e7a932e997
yarikzv <yzv.yaroslav@gmail.com>
2021-10-05 18:42:32 +0300

    lesson14 lombok commented

--- src/lesson14/lesson14.java
+++ src/lesson14/lesson14.java
@@ -2,7 +2,7 @@ package lesson14;
 
 public class lesson14 {
     public static void main(String[] args) {
-        Person p = new Person(10,"Mike", "mike@test.com");
-        System.out.println(p);
+//        Person p = new Person(10,"Mike", "mike@test.com");
+//        System.out.println(p);
     }
 }
